77a64c823fad3b2b8960d0d2651afcf6cf1ff2ee
Saket Singh <salets961@gmail.com>
2026-07-28 15:30:09 +0530

    vite name added to the env

--- src/Socket.js
+++ src/Socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io(import.meta.env.API_URL, {
+export const socket = io(import.meta.env.VITE_API_URL, {
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
