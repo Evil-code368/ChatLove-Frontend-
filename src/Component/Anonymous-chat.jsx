40e0c732e6b546ae65a0411ba5138149d76bc8d8
Saket Singh <salets961@gmail.com>
2026-08-14 01:10:46 +0530

    update of sitemap

--- src/Component/Anonymous-chat.jsx
+++ src/Component/Anonymous-chat.jsx
@@ -197,7 +197,7 @@ const Anonymouschat = () => {
       <div className="bg-slate-800 p-4 flex justify-between items-center shadow">
 
         <div>
-          <link rel="canonical" href="https://www.chatlove.pro/random-chat/anonymous-chat"></link>
+          <link rel="canonical" href="https://www.chatlove.pro/anonymous-chat"></link>
           <h1 className="text-xl font-bold">
             Anonymous Chat
           </h1>
